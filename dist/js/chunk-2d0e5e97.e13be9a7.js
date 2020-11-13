a09fa105ba162f3f654f238794379a4541159e4e
Şems Yılmaz <yilmazsems@gmail.com>
2020-11-13 22:51:07 +0300

    adding dist subtree

---
+++ dist/js/chunk-2d0e5e97.e13be9a7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(e,n,t){"use strict";t.r(n);var o=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("404 - PAGE NOT FOUND")]),t("vs-button",{staticStyle:{"margin-top":"30px"},on:{click:function(n){return e.$router.push({name:"Home"})}}},[e._v(" Go To Homepage ")])],1)},u=[],c=t("2877"),r={},a=Object(c["a"])(r,o,u,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0e5e97.e13be9a7.js.map
