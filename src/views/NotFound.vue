516d8f37b404cef56ed8c2ebb16a6a54c697ab29
Şems Yılmaz <yilmazsems@gmail.com>
2020-11-13 22:49:41 +0300

    404 page added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>404 - PAGE NOT FOUND</h1>
+        <vs-button 
+            @click="$router.push({name: 'Home'})"
+            style="margin-top: 30px;">
+            Go To Homepage
+        </vs-button>
+    </div>
+</template>
